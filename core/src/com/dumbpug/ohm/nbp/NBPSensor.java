b74ed4bdff3877a38b72ec59fa1774de0384b433
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2018-01-17 14:31:56 +0000

    Tidied up NBPSensor

--- core/src/com/dumbpug/ohm/nbp/NBPSensor.java
+++ core/src/com/dumbpug/ohm/nbp/NBPSensor.java
@@ -3,28 +3,32 @@ package com.dumbpug.ohm.nbp;
 import java.util.ArrayList;
 
 /**
- * Sensors attach to Kinetic and Static NBPBox objects.
- * Created by nik on 27/02/16.
+ * A sensor which can be attached to kinematic and static physics boxes.
  */
 public class NBPSensor {
-    // Name of sensor.
+    /* The name of the sensor. */
     private String name;
-    // Parent of sensor, each sensor can have only one parent.
+    /* The box that this sensor is attached to. */
     private NBPBox parent;
-    // Position
-    private float x;
-    private float y;
-    // Size
-    private float width;
-    private float height;
-    // List of intersecting boxes.
+    /* The sensor position. */
+    private float x, y;
+    /* The sensor size. */
+    private float width, height;
+    /* The list of intersecting boxes. */
     private ArrayList<NBPBox> intersectingBoxes;
 
+    /**
+     * Creates a new instance of the NBPSensor class.
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     */
     public NBPSensor(float x, float y, float width, float height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        this.x            = x;
+        this.y            = y;
+        this.width        = width;
+        this.height       = height;
         intersectingBoxes = new ArrayList<NBPBox>();
     }
 
@@ -57,25 +61,78 @@ public class NBPSensor {
         }
     }
 
+    /**
+     * Check whether this sensor is intersecting a box with the specified name.
+     * @param boxName The name of the box to check for.
+     * @return Whether this sensor is intersecting a box with the specified name.
+     */
+    public boolean isIntersecting(String boxName) {
+        for(NBPBox box : this.intersectingBoxes) {
+            if(box.getName().equals(boxName)) {
+                return true;
+            }
+        }
+        // There was no intersecting box with the specified name.
+        return false;
+    }
+
+    /**
+     * Get all of the boxes that are currently intersecting with the sensor.
+     * @return The boxes which are intersecting with the sensor.
+     */
     public ArrayList<NBPBox> getIntersectingBoxes() { return this.intersectingBoxes; }
 
+    /**
+     * Set the box that this sensor is attached to.
+     * @param parent The box that this sensor is attached to.
+     */
     public void setParent(NBPBox parent) { this.parent = parent; }
 
-    public NBPBox getParent() { return this.parent; }
-
+    /**
+     * Get the sensor name.
+     * @return The sensor name.
+     */
     public String getName() { return this.name; }
 
+    /**
+     * Set the sensor name.
+     * @param name The sensor name.
+     */
     public void setName(String name) { this.name = name; }
 
+    /**
+     * Get the x position.
+     * @return The x position.
+     */
     public float getX() { return x; }
 
+    /**
+     * Set the x position.
+     * @param x
+     */
     public void setX(float x) { this.x = x; }
 
+    /**
+     * Get the y position.
+     * @return The y position.
+     */
     public float getY() { return y; }
 
+    /**
+     * Set the y position.
+     * @param y
+     */
     public void setY(float y) { this.y = y; }
 
+    /**
+     * Get the width of the sensor.
+     * @return The width of the sensor.
+     */
     public float getWidth() { return this.width; }
 
+    /**
+     * Get the height of the sensor.
+     * @return the height of the sensor.
+     */
     public float getHeight() { return this.height; }
 }
