3f646bc6884ad7342e39991e3e26ec7b8a715b29
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2018-02-19 13:45:55 +0000

    Added player selection state and using that to pass two players to game state. The need their own input processors.

---
+++ core/src/com/dumbpug/ohm/player/IngamePlayer.java
@@ -0,0 +1,8 @@
+package com.dumbpug.ohm.player;
+
+/**
+ * Created by nikolas.howard on 19/02/2018.
+ */
+
+public class IngamePlayer {
+}
---
+++ core/src/com/dumbpug/ohm/state/states/PlayerSelection.java
@@ -0,0 +1,8 @@
+package com.dumbpug.ohm.state.states;
+
+/**
+ * Created by nikolas.howard on 19/02/2018.
+ */
+
+public class PlayerSelection {
+}
